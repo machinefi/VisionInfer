26a689a91edeaa1445f35510b8db8e0363c9acf1
zhangjianian <iloveyou.gooogle@gmail.com>
2026-03-27 13:24:21 +0800

    Update Version
    Update Version

--- vinfer/__init__.py
+++ vinfer/__init__.py
@@ -3,7 +3,7 @@ from .inference_core import infer_frame
 from .frame_producer import start_frame_producer, stop_frame_producer
 from .backend.ollama_manager import start_ollama_serve, stop_ollama_serve
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = [
     "main", "infer_frame", "start_frame_producer",
     "stop_frame_producer", "start_ollama_serve", "stop_ollama_serve"
